2781bf026a8d38d8ef9b50b65007f0ed5ef5ad4d
vmurali100 <vmurali100@gmail.com>
2020-05-09 21:43:19 +0530

    Adding Project

---
+++ ReactJS/resume_project/src/About.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section
+          className="resume-section p-3 p-lg-5 d-flex align-items-center"
+          id="about"
+        >
+          <div className="w-100">
+            <h1 className="mb-0">
+              Clarence
+              <span className="text-primary">Taylor</span>
+            </h1>
+            <div className="subheading mb-5">
+              3542 Berry Street · Cheyenne Wells, CO 80810 · (317) 585-8468 ·
+              <a href="mailto:name@email.com">name@email.com</a>
+            </div>
+            <p className="lead mb-5">
+              I am experienced in leveraging agile frameworks to provide a
+              robust synopsis for high level overviews. Iterative approaches to
+              corporate strategy foster collaborative thinking to further the
+              overall value proposition.
+            </p>
+            <div className="social-icons">
+              <a href="#">
+                <i className="fab fa-linkedin-in"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-github"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+            </div>
+          </div>
+        </section>
+
+        <hr className="m-0"></hr>
+      </div>
+    );
+  }
+}
---
+++ ReactJS/resume_project/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Nav from "./Nav";
+import About from "./About";
+import Experience from "./Experience";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <About />
+      <Experience />
+    </div>
+  );
+}
+
+export default App;
